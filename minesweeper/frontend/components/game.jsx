edf3ad48732dad45decdcbfedf9a048748f081c0
David Suh <davidisuh93@gmail.com>
2021-01-06 17:35:02 -0500

    trying to finish phase 4

--- minesweeper/frontend/components/game.jsx
+++ minesweeper/frontend/components/game.jsx
@@ -9,6 +9,7 @@ class Game extends React.Component {
         this.state = { board: board };
 
         this.updateGame = this.updateGame.bind(this);
+        this.restartGame = this.restartGame.bind(this);
     }
 
     updateGame(tile, altkey) {
@@ -26,10 +27,28 @@ class Game extends React.Component {
         // debugger
     }
 
+    restartGame() {
+        if(this.state.board.won() || this.state.board.lost()) {
+            this.setState({board: new Minesweeper.Board(10, 10)})
+        }
+        // const board = new Minesweeper.Board(10, 10);
+        // this.setState({ board: board });
+    }
+
     render () {
+        if(this.state.board.won()) {
+            return (<h1>You won!</h1>)
+        } else if (this.state.board.lost()) {
+            return (<h1>You lost!</h1>
+                <button onClick={this.restartGame}>Restart Game</button>)
+        }
+
+
         return (
             <div>
-                <Board board={this.state.board} update={this.updateGame}  />
+                {gameOver.bind(this)}
+                <Board board={this.state.board} update={this.updateGame} />
+                <button onClick={this.restartGame}>Restart Game</button>
             </div>
         )
     }
